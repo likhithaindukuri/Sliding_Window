e2881ec134ef8e954dae8b0080c4457f51ad419b
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-11-20 20:36:19 +0530

    Kadane's algorithm

---
+++ kadanesAlgo.cpp
@@ -0,0 +1,22 @@
+class Solution{
+    public:
+    long long maxSubarraySum(int arr[], int n){
+        
+        long long maxi = LONG_MIN; 
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+    return maxi;
+        
+    }
+};
